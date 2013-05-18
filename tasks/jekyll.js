73d8f8cce653e5211b7857ce1feb080ab2d3fd81
Rob Wierzbowski <hello@robwierzbowski.com>
2013-05-18 02:25:01 -0400

    Code simplification

--- tasks/jekyll.js
+++ tasks/jekyll.js
@@ -7,35 +7,35 @@ module.exports = function (grunt) {
 		// Tell grunt this task is asynchronous.
 		var done = this.async(),
 			exec = require('child_process').exec,
-			command = "jekyll",
+			command = 'jekyll',
 
-			optionList = [
-				{ name : "src", flag : "--source" },
-				{ name : "dest", flag : "--destination" },
-				{ name : "safe", flag : "--safe" },
-				{ name : "plugins", flag : "--plugins" },
-				{ name : "layouts", flag : "--layouts" },
-				{ name : "watch", flag : "--watch" },
-				{ name : "auto", flag : "--watch" },
-				{ name : "config", flag : "--config" },
-				{ name : "drafts", flag : "--drafts" },
-				{ name : "future", flag : "--future" },
-				{ name : "lsi", flag : "--lsi" },
-				{ name : "limit_posts", flag : "--limit_posts" },
-				{ name : "port", flag : "--port" },
-				{ name : "server_port", flag : "--port" },
-				{ name : "host", flag : "--host" },
-				{ name : "baseurl", flag : "--baseurl" },
+			optionList = {
+				'src': '--source',
+				'dest': '--destination',
+				'safe': '--safe',
+				'plugins': '--plugins',
+				'layouts': '--layouts',
+				'watch': '--watch',
+				'auto': '--watch',
+				'config': '--config',
+				'drafts': '--drafts',
+				'future': '--future',
+				'lsi': '--lsi',
+				'limit_posts': '--limit_posts',
+				'port': '--port',
+				'server_port': '--port',
+				'host': '--host',
+				'baseurl': '--baseurl',
 
 				// deprecated flags
-				{ name : "paginate", flag : false },
-				{ name : "permalink", flag : false },
-				{ name : "markdown", flag : false },
-				{ name : "url", flag : false }
-			],
+				'paginate': false,
+				'permalink': false,
+				'markdown': false,
+				'url': false
+			},
 
-      // Support 'options' and bare style configuration
-      options = Object.keys(this.options()).length ? this.options() : this.data;
+		// Support 'options' and bare style configuration
+		options = Object.keys(this.options()).length ? this.options() : this.data;
 
 		if (options.bundleExec) {
 			command = 'bundle exec ' + command;
@@ -47,21 +47,17 @@ module.exports = function (grunt) {
 			command += ' build';
 		}
 
-		optionList.forEach(function (option, i) {
-
-			var userOption = options[optionList[i].name];
-
-			if (userOption) {
-				if (option.flag) {
-					command += ' ' + option.flag;
-					if (typeof userOption !== 'boolean') {
-						command += ' ' + userOption;
-					}
-				} else {
-					grunt.warn('`' + option.name + '` has been deprecated. You may want to try this option in the configuration file.');
+		// Add optionList options to command
+		Object.keys(optionList).forEach(function (option) {
+			if (options[option]) {
+				command += ' ' + optionList[option];
+				if (typeof options[option] !== 'boolean') {
+					command += ' ' + options[option];
+				}
+				if (!options[option]) {
+					grunt.warn('`' + option + '` has been deprecated. You may want to try this option in the configuration file.');
 				}
 			}
-
 		});
 
 		function puts(error, stdout, stderr) {
